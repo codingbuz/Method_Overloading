e0b026b53d2b5a7926c519db114a42f2b258537d
upendra kumar <kupendraraj144@gmail.com>
2023-05-11 12:31:30 +0530

    overriding

---
+++ src/com/driver/A.java
@@ -0,0 +1,13 @@
+
+
+package com.driver;
+public class A {
+public A(){}
+   public String meth(){
+        String a="raja";
+       return a;
+    }
+public static void main(String[] args){}
+
+}
+
---
+++ src/com/driver/B.java
@@ -0,0 +1,12 @@
+package com.driver;
+public class B extends A{
+    public String meth(){
+        String b="rani";
+    return b;
+
+}
+public static void main(String[] args) {
+    B a = new B();
+    a.meth();
+}
+}
